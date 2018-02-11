741da437d6cbdd5d81109726e60bcae3755903d9
aleksandr <shuto4ka1@gmail.com>
2018-02-11 14:00:41 +0300

    Fixed testcontainers. Minor refactor.

--- backend/src/test/java/my/own/linkaggregator/service/UserServiceImplTest.java
+++ backend/src/test/java/my/own/linkaggregator/service/UserServiceImplTest.java
@@ -2,20 +2,12 @@ package my.own.linkaggregator.service;
 
 import my.own.linkaggregator.domain.User;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static my.own.linkaggregator.TestData.USER_1;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-public class UserServiceImplTest {
+public class UserServiceImplTest extends AbstactDbTest {
     @Autowired
     UserService userService;
 
